6ff8d967aee6ae9e1688f2882e39bdebaa3fe99a
JasonEb <jasonebueng@gmail.com>
2020-10-18 18:25:36 -0700

    Updates

---
+++ precache-manifest.154cecae4ba43ae93b4c7a7d6ec045d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b296d7eb033204b43c25935283291135",
+    "url": "/chaturstrations/index.html"
+  },
+  {
+    "revision": "278473555e1114da33af",
+    "url": "/chaturstrations/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8989a8ac8fb0d99afb91",
+    "url": "/chaturstrations/static/js/2.dba6357a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/chaturstrations/static/js/2.dba6357a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "278473555e1114da33af",
+    "url": "/chaturstrations/static/js/main.6d603250.chunk.js"
+  },
+  {
+    "revision": "3a8c4d8eaa9837e729a7",
+    "url": "/chaturstrations/static/js/runtime-main.031dbd29.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chaturstrations/static/media/logo.5d5d9eef.svg"
+  }
+]);
